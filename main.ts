de1d8044bdebf5741c317f2c9888b285d9555e79
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-08 10:01:05 -0400

    final commit

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let potentiometer = robotbit.RgbUltrasonic(DigitalPin.P1)
+basic.showIcon(IconNames.Yes)
+basic.pause(500)
 basic.forever(function () {
-	
+    basic.showNumber(potentiometer)
+    basic.pause(500)
 })
