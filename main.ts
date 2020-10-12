01825632f2058b413b310c0773ea30f8d8a4c5ce
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-10-12 14:09:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
+// Created by: Mikayla Barthelette
+// Created on: Oct 2020
+// 
+// This program allows us to use a potentiometer to control the brightness of an LED
 let potentiometer = 0
 basic.showIcon(IconNames.Yes)
 basic.pause(1000)
 basic.forever(function () {
     potentiometer = pins.analogReadPin(AnalogPin.P1)
+    pins.analogWritePin(AnalogPin.P13, pins.analogReadPin(AnalogPin.P1))
     basic.showNumber(potentiometer)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    basic.pause(1000)
 })
